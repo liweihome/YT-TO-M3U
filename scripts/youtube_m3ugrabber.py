f2c877cd54096d285f7ad46dae411c227a53ada7
liweihome <122725488+liweihome@users.noreply.github.com>
2024-07-23 10:05:19 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -22,16 +22,16 @@ if 'win' in sys.platform:
 
 def grab(url):
     response = requests.get(url, timeout=15).text
-    if '.m3u8' not in response:
+    if '.m3u' not in response:
         #response = requests.get(url).text
-        if '.m3u8' not in response:
+        if '.m3u' not in response:
             if windows:
                 print('https://raw.githubusercontent.com/liweihome/YT-TO-M3U/main/assets/moose_na.m3u')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
-            if '.m3u8' not in response:
+            if '.m3u' not in response:
                 print('https://raw.githubusercontent.com/liweihome/YT-TO-M3U/main/assets/moose_na.m3u')
                 return
     end = response.find('.m3u8') + 5
@@ -40,7 +40,7 @@ def grab(url):
         if 'https://' in response[end-tuner : end]:
             link = response[end-tuner : end]
             start = link.find('https://')
-            end = link.find('.m3u8') + 5
+            end = link.find('.m3u') + 5
             break
         else:
             tuner += 5
